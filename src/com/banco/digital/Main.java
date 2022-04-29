b6fcd931e2039f7279213ec6ed06441f77b68b3a
DeVector <diniz.vito@gmail.com>
2022-04-29 17:33:34 -0300

    Adicionando a classe main

---
+++ src/com/banco/digital/Main.java
@@ -0,0 +1,9 @@
+package com.banco.digital;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("---------------- Bem vindo ao banco Digital VDBR ---------------------");
+    }
+
+}
